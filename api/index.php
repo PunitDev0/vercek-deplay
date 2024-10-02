911a61040573a442d40fdd18db515815807432fa
PunitDev0 <punitdeveloper1@tgmail.com>
2024-10-03 02:51:24 +0530

    all done

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+// api/index.php
+
+// Set the correct content type
+header('Content-Type: text/html; charset=utf-8');
+
+// Include your routing logic here
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Simple routing logic to include the appropriate PHP file
+if (preg_match('/^\/admin/', $requestUri)) {
+    include(__DIR__ . '/../admin/pages' . substr($requestUri, 6));
+} elseif (preg_match('/^\/pages/', $requestUri)) {
+    include(__DIR__ . '/../pages' . substr($requestUri, 6));
+} elseif (preg_match('/^\/images/', $requestUri)) {
+    // Serve images directly from the images folder
+    $imagePath = __DIR__ . '/../images' . substr($requestUri, 6);
+    if (file_exists($imagePath)) {
+        header('Content-Type: image/jpeg');
+        readfile($imagePath);
+        exit();
+    }
+} else {
+    // Default to your main page or a 404 page
+    include(__DIR__ . '/../pages/home.php'); // Change this to your main page
+}
