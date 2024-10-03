c88bfc87845216a52dd951eb6f30b5dcdaaba5e2
PunitDev0 <punitdeveloper1@tgmail.com>
2024-10-03 18:22:17 +0530

    update code

--- api/index.php
+++ api/index.php
@@ -1,10 +1,26 @@
 <?php
 // api/index.php
 
-header("Content-Type: application/json");
+// Set the correct content type
+header('Content-Type: text/html; charset=utf-8');
 
-$data = [
-    'message' => 'Hello, World!',
-];
+// Include your routing logic here
+$requestUri = $_SERVER['REQUEST_URI'];
 
-echo json_encode($data);
+// Simple routing logic to include the appropriate PHP file
+if (preg_match('/^\/admin/', $requestUri)) {
+    include(__DIR__ . '/../admin/pages' . substr($requestUri, 6));
+} elseif (preg_match('/^\/pages/', $requestUri)) {
+    include(__DIR__ . '/../pages' . substr($requestUri, 6));
+} elseif (preg_match('/^\/images/', $requestUri)) {
+    // Serve images directly from the images folder
+    $imagePath = __DIR__ . '/../images' . substr($requestUri, 6);
+    if (file_exists($imagePath)) {
+        header('Content-Type: image/jpeg');
+        readfile($imagePath);
+        exit();
+    }
+} else {
+    // Default to your main page or a 404 page
+    include(__DIR__ . '/../pages/home.php'); // Change this to your main page
+}
